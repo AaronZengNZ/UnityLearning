ef11c3fbf09b6fc5dece446479733b0613399445
Aaron Zeng <aaron@10168.com>
2022-06-19 16:59:22 +1200

    Started making third person perspective

--- Unity/RelmRushe/Assets/Scripts/Waypoint.cs
+++ Unity/RelmRushe/Assets/Scripts/Waypoint.cs
@@ -7,18 +7,19 @@ public class Waypoint : MonoBehaviour
     [SerializeField] Tower tower;
     [SerializeField] GameObject towerPrefab;
     [SerializeField] bool isPlaceable = false;
+    bool perspectiveIsThirdPerson = false;
 
     public bool IsPlaceable { get { return isPlaceable; } }
 
     // Start is called before the first frame update
-   // void OnMouseDown()
-    //{
-    //    if (isPlaceable)
-    //    {
-    //        bool isPlaced = tower.CreateTower(towerPrefab, transform.position);
-   ///         isPlaceable = !isPlaced;
-   //     }
-///}   
+   void OnMouseDown()
+   {
+       if (isPlaceable && perspectiveIsThirdPerson)
+       {
+          bool isPlaced = tower.CreateTower(towerPrefab, transform.position);
+           isPlaceable = !isPlaced;
+       }
+   }   
 
     void OnCollisionStay()
     {
